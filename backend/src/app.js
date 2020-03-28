5b4f06ae1e8fa1db7a5554644ce4c88f94ee85ba
matheusap1 <62668698+matheusap1@users.noreply.github.com>
2020-03-28 12:53:14 -0300

    Back-End da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const { errors } = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,9 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
-app.listen(3333);
+module.exports = app;
 
 
 
